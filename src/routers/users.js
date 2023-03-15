5909854d402298a2b23b370ab41995aa9ffd77d0
barrionuevoJ <julianbarrionuevo10@gmail.com>
2023-03-15 13:05:54 -0300

    Algunos inputs agregados y acomodados, sin validación

--- src/routers/users.js
+++ src/routers/users.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const router = express.Router();
+
 const userController = require ('../controllers/userController')
+const multerMiddleware = require('../middleware/multer');
+const uploadFile = multerMiddleware('users', 'user');
+const userRegisterValidation = require('../middleware/userRegisterValidation')
 
 router.get('/login', userController.login);
 
-router.get('/register', userController.register);
+router.get('/register', uploadFile.single('img'),userController.register);
 
 module.exports = router;
