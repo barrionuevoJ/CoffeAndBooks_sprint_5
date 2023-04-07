27e53b39cb7742a6d0e138feb38ba5ef659e8b6e
barrionuevoJ <julianbarrionuevo10@gmail.com>
2023-04-06 22:46:26 -0300

    Imagen de fondo agregada

--- src/routers/users.js
+++ src/routers/users.js
@@ -1,16 +1,23 @@
 const express = require('express')
 const router = express.Router();
-
 const userController = require('../controllers/userController')
-const multerMiddleware = require('../middleware/multer');
-const uploadFile = multerMiddleware('users', 'user');
-const userRegisterValidation = require('../middleware/userRegisterValidation')
 
+const multerMiddleware = require('../middleware/multer');
+const userRegisterValidation = require('../middleware/userRegisterValidation')
+const guestMiddleware = require('../middleware/guestMiddleware')
+const uploadFile = multerMiddleware('users', 'user');
+
+// Formulario de login
 router.get('/login', userController.login);
+
+//Perfil de usuario
 router.get('/profile/:id', userController.profile);
 
 // Formulario de registro de usuario
 router.get('/register', userController.register);
+// router.get('/register', guestMiddleware, userController.register);
+
+// Procesar el registro 
 router.post('/register', uploadFile.single('profileImg'), userRegisterValidation, userController.newUser);
 
 // Carrito
