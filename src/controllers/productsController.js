93e981dad7e774767404e8e08dd6f7d92be31709
BarrionuevoJ <julianbarrionuevo10@gmail.com>
2023-05-09 21:12:15 -0300

    CRUD terminado, esperando para comprobar errores

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,35 +1,32 @@
 const jsonDB = require("../model/jsonDatabase");
-const productModel = jsonDB("products");
+// const productModel = jsonDB("products");
 const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-const fs = require("fs");
-const path = require("path");
+// const fs = require("fs");
+// const path = require("path");
 
-const db = require("../database/models");
+// const db = require("../database/models");
 
-const { Producto } = require("../database/models");
+const { Producto, Genero, Autor, Categoria } = require("../database/models");
 
-const { validationResult } = require("express-validator");
+// const { validationResult } = require("express-validator"); 
 
 const controlador = {
-  db: (req, res) => {
-    Producto.findAll().then((productos) => {
-      res.send(productos);
-    });
-  },
-
-  // Mostrar todos los productos
-  all: (req, res) => {
-    const listaProductos = productModel.all();
-    res.render("products/productList", { lista: listaProductos, toThousand });
-  },
-
-  // Mostrar un producto
-  detail: (req, res) => {
-    const product = productModel.find(req.params.id);
-    res.render("products/productDetail", { libro: product, toThousand });
-  },
 
   //  Sprint 5
+
+
+  // Mostrar todos los productos
+  // all: (req, res) => {
+  //   const listaProductos = productModel.all();
+  //   res.render("products/productList", { lista: listaProductos, toThousand });
+  // },
+
+  // Mostrar un producto
+  // detail: (req, res) => {
+  //   const product = productModel.find(req.params.id);
+  //   res.render("products/productDetail", { libro: product, toThousand });
+  // },
+
 
   // Guardar un producto - Sprint 5
 
@@ -59,39 +56,58 @@ const controlador = {
 
   // Editar un producto - sprint 5
 
-  edit: (req, res) => {
-    let productToEdit = productModel.find(req.params.id);
-    res.render("products/formEdit", { libro: productToEdit });
-  },
+  // edit: (req, res) => {
+  //   let productToEdit = productModel.find(req.params.id);
+  //   res.render("products/formEdit", { libro: productToEdit });
+  // },
 
   // Actualizar un producto
-  
-  update: (req, res) => {
-    let productToEdit = productModel.find(req.params.id);
 
-    productToEdit = {
-      id: productToEdit.id,
-      ...req.body,
-      img: productToEdit.img,
-    };
+  // update: (req, res) => {
+  //   let productToEdit = productModel.find(req.params.id);
 
-    productModel.update(productToEdit);
-    res.redirect("/products/detail/" + req.params.id);
-  },
+  //   productToEdit = {
+  //     id: productToEdit.id,
+  //     ...req.body,
+  //     img: productToEdit.img,
+  //   };
+
+  //   productModel.update(productToEdit);
+  //   res.redirect("/products/detail/" + req.params.id);
+  // },
 
   // Eliminar un producto
 
-  destroy: function (req, res) {
-    productModel.delete(req.params.id);
-    res.redirect("/products/");
+  // destroy: function (req, res) {
+  //   productModel.delete(req.params.id);
+  //   res.redirect("/products/");
+  // },
+  
+  db: (req, res) => {
+    Producto.findAll().then((productos) => {
+      res.send(productos);
+    });
   },
 
+  all: (req, res) => {
+    Producto.findAll({
+      include: ["autor", "genero", "categoria"],
+    }).then((productos) => {
+      res.render("products/productList", { productos, toThousand });
+    });
+  },
+  detail: (req, res) => {
+    Producto.findByPk(req.params.id, {
+      include: ["autor", "genero", "categoria"],
+    }).then((producto) => {
+      res.render("products/productDetail", { producto, toThousand });
+    });
+  },
 
-  // Sprint 6
   add: (req, res) => {
-    let promGeneros = Generos.findAll();
-    let promAutores = Autores.findAll();
-    let promCategorias = Categorias.findAll();
+    let promGeneros = Genero.findAll();
+    let promAutores = Autor.findAll();
+    let promCategorias = Categoria.findAll();
 
     Promise.all([promGeneros, promAutores, promCategorias])
       .then(([generos, autores, categorias]) => {
@@ -119,7 +135,60 @@ const controlador = {
       .catch((error) => res.send(error));
   },
 
-  // Formulario de edicion de un producto
+  // Formulario de edición
+
+  edit: function (req, res) {
+    let idProducto = req.params.id;
+    let promProducto = Producto.findByPk(idProducto, {
+      include: ["genero", "autor", "categoria"],
+    });
+    let promGeneros = Genero.findAll();
+    let promAutores = Autor.findAll();
+    let promCategorias = Categoria.findAll();
+    Promise.all([promProducto, promGeneros, promAutores, promCategorias])
+      .then(([Producto, Generos, Autores, Categorias]) => {
+        return res.render("products/formEdit", {
+          Producto,
+          Generos,
+          Autores,
+          Categorias,
+        });
+      })
+      .catch((error) => res.send(error));
+  },
+
+  // Actualizar un producto
+  update: function (req, res) {
+    let idProducto = req.params.id;
+    Producto.update(
+      {
+        titulo: req.body.titulo,
+        descripcion: req.body.descripcion,
+        cantidad: req.body.cantidad,
+        precio: req.body.precio,
+        img: req.body.img,
+        descuento: req.body.descuento,
+      },
+      {
+        where: { id: idProducto },
+      }
+    )
+      .then(() => {
+        return res.redirect("/products");
+      })
+      .catch((error) => res.send(error));
+  },
+
+  // Eliminar un producto
+
+  destroy: function (req, res) {
+    let idProducto = req.params.id;
+    Producto.destroy({ where: { id: idProducto }, force: true }) // force: true es para asegurar que se ejecute la acción
+      .then(() => {
+        return res.redirect("/products");
+      })
+      .catch((error) => res.send(error));
+  },
 };
 
 module.exports = controlador;
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,131 +1,307 @@
-const jsonDB = require('../model/jsonDatabase');
-const fs = require('fs');
-const path = require('path');
-const bcrypt = require('bcryptjs');
-const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+const jsonDB = require("../model/jsonDatabase");
+const fs = require("fs");
+const path = require("path");
+const bcrypt = require("bcryptjs");
+const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
-const { validationResult } = require('express-validator');
-const userModel = jsonDB('users')
-const productModel = jsonDB('products')
+const { Usuario } = require("../database/models");
+
+const { validationResult } = require("express-validator");
+const userModel = jsonDB("users");
+const productModel = jsonDB("products");
 
 const controlador = {
-    profile: (req, res) => {
-        return res.render('users/profile', {
-            user: req.session.userLogged
-        });
-    },
+  // Sprint 5
 
-    login: (req, res) => {
-        res.render('users/login', {});
-    },
-    loginProcess: (req, res) => {
-        let userToLogin = userModel.findByField('email', req.body.email);
+  // profile: (req, res) => {
+  //     return res.render('users/profile', {
+  //         user: req.session.userLogged
+  //     });
+  // },
 
+  // login: (req, res) => {
+  //     res.render('users/login', {});
+  // },
+  // loginProcess: (req, res) => {
+  //     let userToLogin = userModel.findByField('email', req.body.email);
+
+  //     if (userToLogin) {
+  //         let isOkThePassword = bcrypt.compareSync(req.body.password, userToLogin.password);
+  //         if (isOkThePassword) {
+  //             delete userToLogin.password;
+  //             req.session.userLogged = userToLogin;
+
+  //             if (req.body.checkUser) {
+  //                 res.cookie('userEmail', req.body.email, { maxAge: (1000 * 60) * 60 })
+  //             }
+
+  //             return res.redirect('/');
+  //         }
+  //         return res.render('users/login', {
+  //             errors: {
+  //                 email: {
+  //                     msg: 'Las credenciales no son válidas'
+  //                 }
+  //             }
+  //         });
+  //     }
+
+  //     return res.render('users/login', {
+  //         errors: {
+  //             email: {
+  //                 msg: 'no se encuentra este email en nuestra base de datos'
+  //             }
+  //         }
+  //     });
+  // },
+
+  // logout: (req, res) => {
+  //     req.session.destroy();
+  //     res.clearCookie('userEmail');
+  //     return res.redirect('/users/login');
+  // },
+
+  // userDestroy: function (req, res) {
+  //     const user = userModel.find(req.params.id)
+  //     if (user.profileimg != 'default-user.png') {
+  //         fs.unlinkSync(
+  //             path.resolve(__dirname, '../../public/images/users/' + user.profileimg)
+  //         )
+  //     }
+  //     userModel.delete(user.id);
+  //     if (req.session.userLogged.id == user.id) {
+  //         res.locals.isLogged = false;
+  //     }
+  //     else {
+  //         req.cookies.userEmail = res.cookie('userEmail', req.session.userLogged.email, { maxAge: (1000 * 60) * 60 })
+  //     }
+  //     return res.redirect('/users/usersList')
+  // },
+
+  // userList: (req, res) => {
+  //     const users = userModel.all();
+  //     res.render('users/usersList', { users })
+  // },
+
+  // cart: (req, res) => {
+  //     const carrito = productModel.carrito()
+  //     res.render('users/productCart', { carrito, toThousand })
+  // },
+
+  // addCart: (req, res) => {
+  //     res.redirect('/users/productCart')
+  // },
+
+  // Formulario de registro
+  // register: (req, res) => {
+  //     res.render('users/register', {});
+  // },
+
+  // Proceso de creacion de un nuevo usuario
+  // newUser: (req, res) => {
+  //     let user = req.body;
+  //     let errors = validationResult(req);
+  //     if (errors.isEmpty()) {
+  //         if (userModel.findByField('email', req.body.email)) {
+  //             if (req.file) {
+  //                 fs.unlinkSync(
+  //                     path.resolve(__dirname, '../../public/images/users/' + req.file.filename)
+  //                 )
+  //             }
+  //             let errors = {
+  //                 email: {
+  //                     msg: 'Email existente'
+  //                 }
+  //             }
+  //             delete req.body.password;
+  //             res.render('users/register', { errors, old: req.body });
+  //         } else {
+  //             user.profileimg = req.file ? req.file.filename : 'default-user.png';
+  //             delete user["passwordConfirm"];
+  //             user.password = bcrypt.hashSync(user.password, 10);
+  //             userModel.create(user);
+  //             res.redirect('/users/login/');
+  //         }
+  //     } else {
+  //         if (req.file) {
+  //             fs.unlinkSync(
+  //                 path.resolve(__dirname, '../../public/images/users/' + req.file.filename)
+  //             )
+  //         }
+  //         delete req.body.password;
+  //         res.render('users/register', { errors: errors.mapped(), old: req.body });
+  //     };
+  // }
+
+  profile: (req, res) => {
+    return res.render("users/profile", {
+      user: req.session.userLogged,
+    });
+  },
+
+  login: (req, res) => {
+    res.render("users/login", {});
+  },
+  loginProcess: (req, res) => {
+    Usuario.findOne({
+      where: {
+        email: req.body.email,
+      },
+    })
+      .then((userToLogin) => {
         if (userToLogin) {
-            let isOkThePassword = bcrypt.compareSync(req.body.password, userToLogin.password);
-            if (isOkThePassword) {
-                delete userToLogin.password;
-                req.session.userLogged = userToLogin;
+          let isOkThePassword = bcrypt.compareSync(
+            req.body.password,
+            userToLogin.password
+          );
+          if (isOkThePassword) {
+            delete userToLogin.password;
+            req.session.userLogged = userToLogin;
 
-                if (req.body.checkUser) {
-                    res.cookie('userEmail', req.body.email, { maxAge: (1000 * 60) * 60 })
-                }
-
-                return res.redirect('/');
+            if (req.body.checkUser) {
+              res.cookie("userEmail", req.body.email, {
+                maxAge: 1000 * 60 * 60,
+              });
             }
-            return res.render('users/login', {
-                errors: {
-                    email: {
-                        msg: 'Las credenciales no son válidas'
-                    }
-                }
-            });
-        }
 
-        return res.render('users/login', {
+            return res.redirect("/");
+          }
+          return res.render("users/login", {
             errors: {
-                email: {
-                    msg: 'no se encuentra este email en nuestra base de datos'
-                }
-            }
+              email: {
+                msg: "Las credenciales no son válidas",
+              },
+            },
+          });
+        }
+
+        return res.render("users/login", {
+          errors: {
+            email: {
+              msg: "no se encuentra este email en nuestra base de datos",
+            },
+          },
         });
-    },
+      })
+      .catch((error) => {
+        console.log(error);
+        res.send("Error interno del servidor");
+      });
+  },
 
-    logout: (req, res) => {
-        req.session.destroy();
-        res.clearCookie('userEmail');
-        return res.redirect('/users/login');
-    },
+  logout: (req, res) => {
+    req.session.destroy();
+    res.clearCookie("userEmail");
+    return res.redirect("/users/login");
+  },
 
-    userDestroy: function (req, res) {
-        const user = userModel.find(req.params.id)
-        if (user.profileimg != 'default-user.png') {
-            fs.unlinkSync(
-                path.resolve(__dirname, '../../public/images/users/' + user.profileimg)
+  // Proceso de eliminacion de un usuario
+  userDestroy: function (req, res) {
+    Usuario.findByPk(req.params.id)
+      .then((user) => {
+        if (user.profileimg != "default-user.png") {
+          fs.unlinkSync(
+            path.resolve(
+              __dirname,
+              "../../public/images/users/" + user.profileimg
             )
+          );
         }
-        userModel.delete(user.id);
-        if (req.session.userLogged.id == user.id) {
-            res.locals.isLogged = false;
-        }
-        else {
-            req.cookies.userEmail = res.cookie('userEmail', req.session.userLogged.email, { maxAge: (1000 * 60) * 60 })
-        }
-        return res.redirect('/users/usersList')
-    },
-
-    userList: (req, res) => {
-        const users = userModel.all();
-        res.render('users/usersList', { users })
-    },
-
-    cart: (req, res) => {
-        const carrito = productModel.carrito()
-        res.render('users/productCart', { carrito, toThousand })
-    },
-
-    addCart: (req, res) => {
-        res.redirect('/users/productCart')
-    },
-
-    register: (req, res) => {
-        res.render('users/register', {});
-    },
-
-    newUser: (req, res) => {
-        let user = req.body;
-        let errors = validationResult(req);
-        if (errors.isEmpty()) {
-            if (userModel.findByField('email', req.body.email)) {
-                if (req.file) {
-                    fs.unlinkSync(
-                        path.resolve(__dirname, '../../public/images/users/' + req.file.filename)
-                    )
-                }
-                let errors = {
-                    email: {
-                        msg: 'Email existente'
-                    }
-                }
-                delete req.body.password;
-                res.render('users/register', { errors, old: req.body });
-            } else {
-                user.profileimg = req.file ? req.file.filename : 'default-user.png';
-                delete user["passwordConfirm"];
-                user.password = bcrypt.hashSync(user.password, 10);
-                userModel.create(user);
-                res.redirect('/users/login/');
-            }
+        return Usuario.destroy({ where: { id: user.id } });
+      })
+      .then(() => {
+        if (req.session.userLogged.id == req.params.id) {
+          res.locals.isLogged = false;
         } else {
+          req.cookies.userEmail = res.cookie(
+            "userEmail",
+            req.session.userLogged.email,
+            { maxAge: 1000 * 60 * 60 }
+          );
+        }
+        res.redirect("/users/usersList");
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).send("Hubo un error en el servidor");
+      });
+  },
+
+  userList: (req, res) => {
+    Usuario.findAll().then((usuarios) => {
+      res.render("users/usersList", { usuarios });
+    });
+  },
+
+  cart: (req, res) => {
+    const carrito = productModel.carrito();
+    res.render("users/productCart", { carrito, toThousand });
+  },
+
+  addCart: (req, res) => {
+    res.redirect("/users/productCart");
+  },
+
+  // Formulario de registro
+  register: (req, res) => {
+    res.render("users/register", {});
+  },
+
+  // Proceso de creacion de un nuevo usuario
+  newUser: (req, res) => {
+    let user = req.body;
+    let errors = validationResult(req);
+
+    if (errors.isEmpty()) {
+      Usuario.findOne({ where: { email: req.body.email } })
+        .then((foundUser) => {
+          if (foundUser) {
             if (req.file) {
-                fs.unlinkSync(
-                    path.resolve(__dirname, '../../public/images/users/' + req.file.filename)
+              fs.unlinkSync(
+                path.resolve(
+                  __dirname,
+                  "../../public/images/users/" + req.file.filename
                 )
+              );
             }
+            let errors = {
+              email: {
+                msg: "Email existente",
+              },
+            };
             delete req.body.password;
-            res.render('users/register', { errors: errors.mapped(), old: req.body });
-        };
+            res.render("users/register", { errors, old: req.body });
+          } else {
+            user.profileimg = req.file ? req.file.filename : "default-user.png";
+            delete user["passwordConfirm"];
+            user.password = bcrypt.hashSync(user.password, 10);
+            db.Usuario.create(user)
+              .then(() => {
+                res.redirect("/users/login/");
+              })
+              .catch((error) => {
+                console.log(error);
+                res.status(500).send("Hubo un error en el servidor");
+              });
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          res.status(500).send("Hubo un error en el servidor");
+        });
+    } else {
+      if (req.file) {
+        fs.unlinkSync(
+          path.resolve(
+            __dirname,
+            "../../public/images/users/" + req.file.filename
+          )
+        );
+      }
+      delete req.body.password;
+      res.render("users/register", { errors: errors.mapped(), old: req.body });
     }
-}
+  },
+};
 
 module.exports = controlador;
